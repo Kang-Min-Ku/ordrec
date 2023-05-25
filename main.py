07ce3f631e5dc3dce6d62313345590f9f27547b9
K <netisen4@gmail.com>
2023-05-25 20:19:24 +0900

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 
 from utils.config import load_config
 from utils.data import load_data
+from trainer import Trainer
 
 # Argument
 parser = argparse.ArgumentParser()
@@ -16,4 +17,7 @@ args = parser.parse_args()
 config = load_config(args.config)
 
 # Load data
-num_user, num_item, train_adj, train_unique_users, test_adj, test_unique_users = load_data(config["data_path"])
+num_users, num_items, train_adj, train_unique_users, test_adj, test_unique_users = load_data(config["data_path"])
+
+# 여기부터 하믄 됨
+# trainer = Trainer(config, num_users, num_items, train_adj, test_adj) 
