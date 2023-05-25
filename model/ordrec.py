c06bbab1d608d052f7f37178420582c0b7bb49db
K <netisen4@gmail.com>
2023-05-25 16:36:40 +0900

    change forward func return

--- model/ordrec.py
+++ model/ordrec.py
@@ -2,23 +2,31 @@ from model.GONN import GONN
 import torch
 import torch.nn as nn
 
-class OrdRec():
-    def __init__(self, num_users, num_items, params):
+class OrdRec(nn.Module):
+    def __init__(self, params, num_users, num_items):
+        super(OrdRec, self).__init__()
+
         self.params = params
         self.num_users = num_users
         self.num_items = num_items
         self.num_nodes = self.num_users + self.num_items
 
         self.GONN = GONN(self.params)
-        self.embedding = nn.Embedding(self.num_nodes, self.params['in_channel']) #need initialze
+        self.x = nn.Parameter(torch.empty(self.num_nodes, self.params['in_channel'], dtype=torch.float))
+        #self.embedding = nn.Embedding(self.num_nodes, self.params['in_channel'])
+
+        self.init_embedding()
 
     def init_embedding(self):
-        nn.init.xavier_uniform_(self.embedding.weight)
+        nn.init.xavier_uniform_(self.x)
 
-    def forward(self, node_idx, edge_index):
+    def forward(self, user_idx, edge_index):
         if self.train():
-            assert torch.all(node_idx < self.num_nodes)
+            assert torch.all(user_idx < self.num_users)
+        all_embedding = self.GONN(self.x, edge_index)
 
-        x = self.embedding(node_idx)
-        encode_values = self.GONN(x, edge_index)
-        return encode_values
+        #user_embedding = embedding[:self.num_users]
+        user_embedding = all_embedding[user_idx]
+        item_embedding = all_embedding[self.num_users:]
+
+        return user_embedding, item_embedding
