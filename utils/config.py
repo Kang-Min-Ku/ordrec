394d3e3b8c81d641ad81076499cdf76ede167953
K <netisen4@gmail.com>
2023-05-25 16:34:06 +0900

    in&out dimension to yaml

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,6 @@
 import yaml
 
-def load_config(file, in_channel_dim, out_channel_dim):
+def load_config(file):
     with open(file, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    config["in_channel"] = in_channel_dim
-    config["out_channel"] = out_channel_dim
     return config
