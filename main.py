ef9f4a328b973905b6e735511d222f408360e0e3
Kang-Min-Ku <netisen4@gmail.com>
2023-06-06 07:36:55 +0000

    Correct comment

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ config = load_config(args.config)
 # Load data
 num_users, num_items, train_adj, train_unique_users, valid_data, test_data = load_data(config["data_path"])
 
-# 여기부터 하믄 됨
 trainer = Trainer(config, num_users, num_items, train_adj=train_adj, valid_adj=valid_data, test_adj=test_data)
 trainer.train()
+
+trainer.test()
+trainer.save_model()
